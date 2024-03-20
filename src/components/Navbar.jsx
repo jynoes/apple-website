6ba86d24420eb6254ce3a9c176fceafbca8ce06c
jynoes <89066947+jynoes@users.noreply.github.com>
2024-03-20 20:33:48 +0800

    Finish navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { appleImg, searchImg, bagImg } from '../utils'
+import {navLists } from "../constants"
 
 const Navbar = () => {
   return (
@@ -7,14 +8,14 @@ const Navbar = () => {
         <img src={appleImg} alt="apple" width={14} height={18}/>
 
         <div className='flex flex-1 justify-center max-sm:hidden'>
-          {['Phones', 'Macbooks', 'Tablets'].map((nav) => (
-            <div key={nav}>
+          {navLists.map((nav) => (
+            <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white'>
               {nav}
             </div>
           ))}
         </div>
 
-        <div>
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
           <img src={searchImg} alt='search' width={18} height={18}/>
           <img src={bagImg} alt='bag' width={18} height={18}/>
         </div>
