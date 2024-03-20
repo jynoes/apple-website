74275925172bcca0c03f57af34eb0c2ab2e8e468
jynoes <89066947+jynoes@users.noreply.github.com>
2024-03-20 21:23:03 +0800

    Finish hero

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,13 @@
 
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
