d9560c134b3782fb9645e4faa9daaf336f39e2fb
jynoes <nsabido29@gmail.com>
2024-04-01 00:10:30 +0800

    features

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import Features from "./components/Features"
 
 import * as Sentry from "@sentry/react"
 
 const App = () => {
-
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
