efbb7343e016f43c5aa8f01768c4fea08d28ef42
jynoes <nsabido29@gmail.com>
2024-04-02 23:56:28 +0800

    Update features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -57,7 +57,7 @@ const Features = () => {
                 <div className="feature-text-container">
                     <div className="flex-1 flex-center">
                         <p className="feature-text g_text">
-                            iPhone 15 Pro is {' '} <span className="text-white"></span>
+                            iPhone 15 Pro is {' '} <span className="text-white">the first iPhone to feature an aerospace-grade titanium design,</span>
                         </p>
                     </div>
                 </div>
